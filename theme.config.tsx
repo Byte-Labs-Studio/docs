ace6cff802dc7e4ceab79bb3687a4e8b10864cb3
Xirvin <xander228irvin@gmail.com>
2023-09-22 20:24:17 +1000

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Byte Labs Documentation</span>,
+  logo: <span>Byte Labs</span>,
   project: {
-    link: 'https://docs.byte-labs.net/',
+    link: 'https://github.com/Byte-Labs-Project',
   },
   chat: {
     link: 'https://discord.gg/R8bjTWPCGy',
